a7413e66df91fd07b785600e5e002d18ec0d14f0
destroxer05 <danielmillandiaz05@gmail.com>
2024-11-08 18:03:22 +0100

    Base

---
+++ Base/baseForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace prueba_txtBox
+{
+    partial class baseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(686, 29);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(86, 33);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(686, 91);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(106, 33);
+            this.btnActualizar.TabIndex = 3;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(202, 209);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(554, 229);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // baseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "baseForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.BaseForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Base/baseForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using DataSetLibrary;
+using DataBindingLibrary;
+
+namespace prueba_txtBox
+{
+    public partial class baseForm : Form
+    {
+        public DataSet ds;
+        public bool esNuevo = false;
+        public BaseDataAccess dataAccess;
+        public string TableName;
+        public string querySelect;
+        public ComboBox comboBox;
+
+        public baseForm()
+        {
+            InitializeComponent();
+        }
+
+        protected virtual void BaseForm_Load(object sender, EventArgs e)
+        {
+
+            if (DesignMode) return;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            dataAccess = new BaseDataAccess(connectionString);
+
+            ds = dataAccess.PortarTaula(TableName);
+
+            comboBox.DataSource = ds.Tables[TableName];
+            comboBox.SelectedIndexChanged += comboBox_SelectedIndexChanged;
+
+            CargarDatos();
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    textBox.Validated += ValidarTextBox;
+                }
+            }
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCode = comboBox.SelectedValue.ToString();
+
+            if (!string.IsNullOrEmpty(selectedCode))
+            {
+                querySelect = $"SELECT * FROM {TableName} WHERE {comboBox.ValueMember} = '{selectedCode}'";
+
+                ds = dataAccess.PortarTaula(TableName);
+                CargarDatos(); 
+            }
+        }
+
+
+
+        protected void CargarDatos()
+        {
+            DataBindingHelper.ClearDataBindings(this.Controls); 
+            UpdateTable(); 
+
+            BindTextBoxesToData();
+        }
+
+        private void BindTextBoxesToData()
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox textBox && textBox.Tag != null)
+                {
+                    textBox.DataBindings.Clear();
+                    textBox.DataBindings.Add("Text", ds.Tables[TableName], textBox.Tag.ToString());
+                }
+            }
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            esNuevo = true;
+            DataBindingHelper.ClearDataBindings(this.Controls);
+        }
+
+        protected void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (esNuevo)
+            {
+                AddNewRow();
+                DataBindingHelper.BindControlsToData(this.Controls, ds.Tables[TableName]);
+                esNuevo = false;
+            }
+
+            dataAccess.Actualitzar(ds, querySelect, TableName);
+            ds = dataAccess.PortarTaula(TableName);
+            CargarDatos();
+        }
+
+        protected void AddNewRow()
+        {
+            DataRow newRow = ds.Tables[TableName].NewRow();
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    newRow[textBox.Tag.ToString()] = textBox.Text;
+                }
+            }
+
+            ds.Tables[TableName].Rows.Add(newRow);
+        }
+
+        protected void UpdateTable()
+        {
+            dataGridView1.DataSource = ds.Tables[TableName];
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        protected void ValidarTextBox(object sender, EventArgs e)
+        {
+            if (!esNuevo)
+            {
+                ((TextBox)sender).DataBindings[0].BindingManagerBase.EndCurrentEdit();
+            }
+        }
+    }
+}   
