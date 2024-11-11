a5bf585578e1d81c26cca74aac89754625b61b21
samuelShadov <samuel.shadov@comb.cat>
2024-11-11 20:20:58 +0100

    formbase funciona

--- Species/frmSpecies.cs
+++ Species/frmSpecies.cs
@@ -15,11 +15,21 @@ namespace Species
     {
         public frmSpecies()
         {
-            TableName = "Species";
-            querySelect = $"SELECT * FROM {TableName}";
-            //swTextox.Tag = swTextox.nombreColumna;
-            //swTextox2.Tag = swTextox2.nombreColumna;
             InitializeComponent();
+            TableName = "Species";
+            querySelect = "SELECT * FROM Species";
+            swTextBox1.Tag = swTextBox1.nombreColumna;
+            swTextBox2.Tag = swTextBox2.nombreColumna;
+
+            // Asigna el ComboBox en el formulario derivado al ComboBox en la clase base
+            comboBox = comboBox1;  // Asignamos comboBox1 de frmSpecies a la propiedad comboBox de baseForm
+        }
+
+
+        protected override void BaseForm_Load(object sender, EventArgs e)
+        {
+            base.BaseForm_Load(sender, e);
+            comboBox1.DataSource = ds.Tables[TableName];
         }
     }
 }
