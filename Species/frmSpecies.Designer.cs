7c19dd8c585634f12b0693ec75982f82b169a3d1
destroxer05 <danielmillandiaz05@gmail.com>
2024-11-11 19:30:20 +0100

    Species añadido to Pruebas

---
+++ Species/frmSpecies.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Species
+{
+    partial class frmSpecies
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.swTextox2 = new Species.SWTextox();
+            this.swTextox = new Species.SWTextox();
+            this.SuspendLayout();
+            // 
+            // swTextox2
+            // 
+            this.swTextox2.Location = new System.Drawing.Point(48, 152);
+            this.swTextox2.Name = "swTextox2";
+            this.swTextox2.nombreColumna = "descSpecie";
+            this.swTextox2.Size = new System.Drawing.Size(239, 26);
+            this.swTextox2.TabIndex = 6;
+            // 
+            // swTextox
+            // 
+            this.swTextox.Location = new System.Drawing.Point(48, 82);
+            this.swTextox.Name = "swTextox";
+            this.swTextox.nombreColumna = "codeSpecie";
+            this.swTextox.Size = new System.Drawing.Size(239, 26);
+            this.swTextox.TabIndex = 5;
+            // 
+            // frmSpecies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 567);
+            this.Controls.Add(this.swTextox2);
+            this.Controls.Add(this.swTextox);
+            this.Name = "frmSpecies";
+            this.Controls.SetChildIndex(this.swTextox, 0);
+            this.Controls.SetChildIndex(this.swTextox2, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SWTextox swTextox;
+        private SWTextox swTextox2;
+    }
+}
+
---
+++ Species/frmSpecies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using prueba_txtBox;
+
+namespace Species
+{
+    public partial class frmSpecies : prueba_txtBox.baseForm
+    {
+        public frmSpecies()
+        {
+            TableName = "Species";
+            querySelect = $"SELECT * FROM {TableName}";
+            //swTextox.Tag = swTextox.nombreColumna;
+            //swTextox2.Tag = swTextox2.nombreColumna;
+            InitializeComponent();
+        }
+    }
+}
