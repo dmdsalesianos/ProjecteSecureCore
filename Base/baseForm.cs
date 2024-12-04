51f78f2522e9ce4ef16ebeab706dd1e7ebd663ac
destroxer05 <danielmillandiaz05@gmail.com>
2024-12-04 17:29:06 +0100

    frmPlanets falta que funcione idNative

--- Base/baseForm.cs
+++ Base/baseForm.cs
@@ -27,7 +27,7 @@ namespace Base
         {
             if (DesignMode) return;
 
-            string connectionString = "Data Source=sqlserver.S2AM.sdslab.cat;Initial Catalog=SecureCoreG4;Persist Security Info=True;User ID=G4;Password=12345G4aA2425.";
+            string connectionString = ConfigurationManager.ConnectionStrings["ConexioStr"].ConnectionString;
             dataAccess = new MantenimentDades(connectionString);
 
             ds = dataAccess.PortarTaula(TableName);
