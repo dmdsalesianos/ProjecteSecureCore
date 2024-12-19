468a5a390c95d2fb33ce9f2acc279628b67fde6c
OriolAbad <oriol.abad@sarria.salesians.cat>
2024-12-19 17:22:23 +0100

    commit

--- MenuOptions/frmMenuOptions.Designer.cs
+++ MenuOptions/frmMenuOptions.Designer.cs
@@ -48,9 +48,10 @@ namespace MenuOptions
             // 
             // pictureBox_icono
             // 
-            this.pictureBox_icono.Location = new System.Drawing.Point(46, 70);
+            this.pictureBox_icono.Location = new System.Drawing.Point(41, 56);
+            this.pictureBox_icono.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox_icono.Name = "pictureBox_icono";
-            this.pictureBox_icono.Size = new System.Drawing.Size(132, 121);
+            this.pictureBox_icono.Size = new System.Drawing.Size(117, 97);
             this.pictureBox_icono.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_icono.TabIndex = 7;
             this.pictureBox_icono.TabStop = false;
@@ -59,9 +60,9 @@ namespace MenuOptions
             // 
             this.label_acceslevel.AutoSize = true;
             this.label_acceslevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_acceslevel.Location = new System.Drawing.Point(303, 86);
+            this.label_acceslevel.Location = new System.Drawing.Point(269, 69);
             this.label_acceslevel.Name = "label_acceslevel";
-            this.label_acceslevel.Size = new System.Drawing.Size(119, 25);
+            this.label_acceslevel.Size = new System.Drawing.Size(101, 20);
             this.label_acceslevel.TabIndex = 12;
             this.label_acceslevel.Text = "Acces Level";
             // 
@@ -69,9 +70,9 @@ namespace MenuOptions
             // 
             this.label_clase.AutoSize = true;
             this.label_clase.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_clase.Location = new System.Drawing.Point(350, 124);
+            this.label_clase.Location = new System.Drawing.Point(311, 99);
             this.label_clase.Name = "label_clase";
-            this.label_clase.Size = new System.Drawing.Size(73, 25);
+            this.label_clase.Size = new System.Drawing.Size(61, 20);
             this.label_clase.TabIndex = 14;
             this.label_clase.Text = "Classe";
             // 
@@ -79,9 +80,9 @@ namespace MenuOptions
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(362, 162);
+            this.label3.Location = new System.Drawing.Point(322, 130);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.Size = new System.Drawing.Size(48, 20);
             this.label3.TabIndex = 16;
             this.label3.Text = "Form";
             // 
@@ -89,9 +90,9 @@ namespace MenuOptions
             // 
             this.label_color.AutoSize = true;
             this.label_color.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_color.Location = new System.Drawing.Point(362, 200);
+            this.label_color.Location = new System.Drawing.Point(322, 160);
             this.label_color.Name = "label_color";
-            this.label_color.Size = new System.Drawing.Size(59, 25);
+            this.label_color.Size = new System.Drawing.Size(49, 20);
             this.label_color.TabIndex = 18;
             this.label_color.Text = "Color";
             // 
@@ -99,9 +100,9 @@ namespace MenuOptions
             // 
             this.label_ICONO.AutoSize = true;
             this.label_ICONO.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_ICONO.Location = new System.Drawing.Point(68, 42);
+            this.label_ICONO.Location = new System.Drawing.Point(60, 34);
             this.label_ICONO.Name = "label_ICONO";
-            this.label_ICONO.Size = new System.Drawing.Size(78, 25);
+            this.label_ICONO.Size = new System.Drawing.Size(63, 20);
             this.label_ICONO.TabIndex = 21;
             this.label_ICONO.Text = "ICONO";
             // 
@@ -115,9 +116,10 @@ namespace MenuOptions
             this.rjButton_image.FlatAppearance.BorderSize = 0;
             this.rjButton_image.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rjButton_image.ForeColor = System.Drawing.Color.White;
-            this.rjButton_image.Location = new System.Drawing.Point(40, 218);
+            this.rjButton_image.Location = new System.Drawing.Point(36, 174);
+            this.rjButton_image.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rjButton_image.Name = "rjButton_image";
-            this.rjButton_image.Size = new System.Drawing.Size(132, 60);
+            this.rjButton_image.Size = new System.Drawing.Size(117, 48);
             this.rjButton_image.TabIndex = 26;
             this.rjButton_image.Text = "Cambiar Imagen";
             this.rjButton_image.TextColor = System.Drawing.Color.White;
@@ -128,9 +130,9 @@ namespace MenuOptions
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(357, 48);
+            this.label1.Location = new System.Drawing.Point(317, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.Size = new System.Drawing.Size(53, 20);
             this.label1.TabIndex = 27;
             this.label1.Text = "Name";
             // 
@@ -138,11 +140,12 @@ namespace MenuOptions
             // 
             this.swTextbox_Name.EsForanea = false;
             this.swTextbox_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.swTextbox_Name.Location = new System.Drawing.Point(427, 45);
+            this.swTextbox_Name.Location = new System.Drawing.Point(380, 36);
+            this.swTextbox_Name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.swTextbox_Name.Name = "swTextbox_Name";
             this.swTextbox_Name.NomCampBBDD = "Texto";
             this.swTextbox_Name.PermetreBuit = true;
-            this.swTextbox_Name.Size = new System.Drawing.Size(231, 30);
+            this.swTextbox_Name.Size = new System.Drawing.Size(206, 26);
             this.swTextbox_Name.TabIndex = 29;
             this.swTextbox_Name.TipusDada = CustomControls.SWTextbox.Tipus_Dada.Text;
             // 
@@ -150,11 +153,12 @@ namespace MenuOptions
             // 
             this.swTextbox_Rank.EsForanea = false;
             this.swTextbox_Rank.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.swTextbox_Rank.Location = new System.Drawing.Point(427, 83);
+            this.swTextbox_Rank.Location = new System.Drawing.Point(380, 66);
+            this.swTextbox_Rank.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.swTextbox_Rank.Name = "swTextbox_Rank";
             this.swTextbox_Rank.NomCampBBDD = "AccessLevel";
             this.swTextbox_Rank.PermetreBuit = true;
-            this.swTextbox_Rank.Size = new System.Drawing.Size(231, 30);
+            this.swTextbox_Rank.Size = new System.Drawing.Size(206, 26);
             this.swTextbox_Rank.TabIndex = 30;
             this.swTextbox_Rank.TipusDada = CustomControls.SWTextbox.Tipus_Dada.Number;
             // 
@@ -162,57 +166,61 @@ namespace MenuOptions
             // 
             this.swTextbox_Form.EsForanea = false;
             this.swTextbox_Form.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.swTextbox_Form.Location = new System.Drawing.Point(427, 161);
+            this.swTextbox_Form.Location = new System.Drawing.Point(380, 129);
+            this.swTextbox_Form.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.swTextbox_Form.Name = "swTextbox_Form";
             this.swTextbox_Form.NomCampBBDD = "Form";
             this.swTextbox_Form.PermetreBuit = true;
-            this.swTextbox_Form.Size = new System.Drawing.Size(231, 30);
+            this.swTextbox_Form.Size = new System.Drawing.Size(206, 26);
             this.swTextbox_Form.TabIndex = 32;
-            this.swTextbox_Form.TipusDada = CustomControls.SWTextbox.Tipus_Dada.Text;
+            this.swTextbox_Form.TipusDada = CustomControls.SWTextbox.Tipus_Dada.All;
             // 
             // swTextbox_Classe
             // 
             this.swTextbox_Classe.EsForanea = false;
             this.swTextbox_Classe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.swTextbox_Classe.Location = new System.Drawing.Point(427, 123);
+            this.swTextbox_Classe.Location = new System.Drawing.Point(380, 98);
+            this.swTextbox_Classe.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.swTextbox_Classe.Name = "swTextbox_Classe";
             this.swTextbox_Classe.NomCampBBDD = "Clase";
             this.swTextbox_Classe.PermetreBuit = true;
-            this.swTextbox_Classe.Size = new System.Drawing.Size(231, 30);
+            this.swTextbox_Classe.Size = new System.Drawing.Size(206, 26);
             this.swTextbox_Classe.TabIndex = 31;
-            this.swTextbox_Classe.TipusDada = CustomControls.SWTextbox.Tipus_Dada.Text;
+            this.swTextbox_Classe.TipusDada = CustomControls.SWTextbox.Tipus_Dada.All;
             // 
             // swTextbox_Color
             // 
             this.swTextbox_Color.EsForanea = false;
             this.swTextbox_Color.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.swTextbox_Color.Location = new System.Drawing.Point(427, 200);
+            this.swTextbox_Color.Location = new System.Drawing.Point(380, 160);
+            this.swTextbox_Color.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.swTextbox_Color.Name = "swTextbox_Color";
             this.swTextbox_Color.NomCampBBDD = "Color";
             this.swTextbox_Color.PermetreBuit = true;
-            this.swTextbox_Color.Size = new System.Drawing.Size(231, 30);
+            this.swTextbox_Color.Size = new System.Drawing.Size(206, 26);
             this.swTextbox_Color.TabIndex = 33;
-            this.swTextbox_Color.TipusDada = CustomControls.SWTextbox.Tipus_Dada.Text;
+            this.swTextbox_Color.TipusDada = CustomControls.SWTextbox.Tipus_Dada.All;
             // 
             // swtxtImagen
             // 
             this.swtxtImagen.EsForanea = false;
-            this.swtxtImagen.Location = new System.Drawing.Point(63, 114);
+            this.swtxtImagen.Location = new System.Drawing.Point(56, 91);
+            this.swtxtImagen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.swtxtImagen.Name = "swtxtImagen";
             this.swtxtImagen.NomCampBBDD = "Icono";
             this.swtxtImagen.PermetreBuit = true;
-            this.swtxtImagen.Size = new System.Drawing.Size(100, 26);
+            this.swtxtImagen.Size = new System.Drawing.Size(89, 22);
             this.swtxtImagen.TabIndex = 34;
             this.swtxtImagen.TipusDada = CustomControls.SWTextbox.Tipus_Dada.Number;
             this.swtxtImagen.TextChanged += new System.EventHandler(this.swtxtImagen_TextChanged);
             // 
             // frmMenuOptions
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(937, 576);
+            this.ClientSize = new System.Drawing.Size(833, 461);
             this.Controls.Add(this.swTextbox_Color);
             this.Controls.Add(this.swTextbox_Form);
             this.Controls.Add(this.swTextbox_Classe);
@@ -227,6 +235,7 @@ namespace MenuOptions
             this.Controls.Add(this.label_acceslevel);
             this.Controls.Add(this.pictureBox_icono);
             this.Controls.Add(this.swtxtImagen);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmMenuOptions";
             this.RightToLeftLayout = true;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
